99586a1df077b00ec48ed3f063c0e6f052a1468f
Suireyha <Marvink2296@gmail.com>
2026-09-16 12:37:51 +1000

    Preliminary error checking

--- Redd.c
+++ Redd.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <Lex.h>
+#include "Lex.h"
 
 int main(int argc, char** args){
 
+    printf("%d", argc);
+    if(argc != 2){ //For now, take in exactly one file
+        printf("Usage: Redd <filePath.txt>");
+        return 0;
+    }
 
+    //char* filename = args[2];
+    //FILE* codeFile
+
+    return 0;
 }
