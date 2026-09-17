c1f03506954160aa8a692d0c04c5e80e441925eb
Suireyha <Marvink2296@gmail.com>
2026-09-17 20:55:11 +1000

    Added dynamic size for string to read from file

--- Redd.c
+++ Redd.c
@@ -11,12 +11,24 @@ int main(int argc, char** args){
         return 0;
     }
 
-    FILE* codeFile;
-    codeFile = fopen(args[1], "r");
-    if(codeFile == NULL){
+    FILE* file;
+    file = fopen(args[1], "r");
+
+    if(file == NULL){
         printf("Error openning file :(\n");
         return 0;
     }
+
+    fseek(file, 0, SEEK_END); //Move file pointer to end of file
+    long size = ftell(file); //Apparently this give me the size of file in bytes
+    rewind(file);
+
+    char *incode = malloc(size + 1); //String the exact size of file pllus null term
+    fread(incode, 1, size, file);
+    incode[size] = '\0';
+
+    printf("%s", incode);
+
 
     return 0;
 }
